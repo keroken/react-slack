692bb048aca5f241fbd7dfb9875a8869b54ac20e
keroken <kenichi.shinagawa@gmail.com>
2021-09-03 22:55:11 +0900

    create base routes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+);
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
