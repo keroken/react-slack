23a62f08d063b07538131d7696efc3887c778584
keroken <kenichi.shinagawa@gmail.com>
2021-09-03 23:04:58 +0900

    create Auth/Login and Register and add routes

--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
 const Root = () => (
   <Router>
     <Switch>
-      <Route path="/" component={App} />
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </Switch>
   </Router>
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
