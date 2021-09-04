7643ef97e1845e9047de61af374bd27963efc67f
keroken <kenichi.shinagawa@gmail.com>
2021-09-04 23:59:59 +0900

    save user to realtime database

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import firebase from '../../firebase';
+import md5 from 'md5';
 
 class Register extends React.Component {
   state = {
@@ -10,8 +11,9 @@ class Register extends React.Component {
     password: '',
     passwordConfirmation: '',
     errors: [],
-    loading: false
-  };
+    loading: false,
+    usersRef: firebase.database().ref('users')
+  }
 
   isFormValid = () => {
     let errors = [];
@@ -48,7 +50,7 @@ class Register extends React.Component {
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
-  };
+  }
 
   handleSubmit = event => {
     event.preventDefault();
@@ -59,14 +61,34 @@ class Register extends React.Component {
         .createUserWithEmailAndPassword(this.state.email, this.state.password)
         .then(createdUser => {
           console.log(createdUser);
-          this.setState({ loading: false });
+          createdUser.user.updateProfile({
+            displayName: this.state.username,
+            photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`
+          })
+          .then(() => {
+            this.saveUser(createdUser).then(() => {
+              console.log('user saved');
+            });
+            this.setState({ username: '', email: '', password: '', passwordConfirmation: '', loading: false });
+          })
+          .catch(err => {
+            console.log(err);
+            this.setState({ errors: this.state.errors.concat(err), loading: false });
+          })
         })
         .catch(err => {
           console.log(err);
           this.setState({ errors: this.state.errors.concat(err), loading: false });
         });
     }
-  };
+  }
+
+  saveUser = createdUser => {
+    return this.state.usersRef.child(createdUser.user.uid).set({
+      name: createdUser.user.displayName,
+      avatar: createdUser.user.photoURL
+    });
+  }
 
   handleInputError = (errors, inputName) => {
     return errors.some(error =>
@@ -146,8 +168,8 @@ class Register extends React.Component {
           </Message>
         </Grid.Column>
       </Grid>
-    );
-  };
+    )
+  }
 }
 
 export default Register;
