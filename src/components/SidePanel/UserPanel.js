cf5080cf6bfd8562ccf71c09ae56a1fec625ca7b
keroken <kenichi.shinagawa@gmail.com>
2021-09-05 23:53:58 +0900

    build SidePanel - Header, UserPanel, Dropdown

---
+++ src/components/SidePanel/UserPanel.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Dropdown, Grid, Header, Icon } from 'semantic-ui-react';
+
+class UserPanel extends React.Component {
+  state = {
+    user: this.props.currentUser
+  }
+
+  dropdownOptions = () => [
+    {
+      key: 'user',
+      text: <span>Signed in as <strong>User</strong></span>,
+      disabled: true
+    }, 
+    {
+      key: 'avatar',
+      text: <span>Change Avatar</span>
+    },
+    {
+      key: 'signout',
+      text: <span>Sign Out</span>
+    }
+  ];
+
+  render() {  
+    return (
+      <Grid style={{ background: '#4c3c4c' }}>
+        <Grid.Column>
+          <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
+            {/* App Header */}
+            <Header inverted floated="left"as="h2">
+              <Icon name="code" />
+              <Header.Content>DevChat</Header.Content>
+            </Header>        
+           {/* User Dropdown */}
+            <Header inverted style={{ padding: '0.25em' }} as="h4">
+            <Dropdown trigger={
+                <span>User</span>
+              } options={this.dropdownOptions()} />
+            </Header>
+          </Grid.Row>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default UserPanel;
