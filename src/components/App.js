4c648da262dcd0c15ba241258baf0072da1c8aa2
keroken <kenichi.shinagawa@gmail.com>
2021-09-05 23:37:49 +0900

    scaffold App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
+import ColorPanel from '../components/ColorPanel/ColorPanel';
+import Messages from '../components/Messages/Messages';
+import MetaPanel from '../components/MetaPanel/MetaPanel';
+import SidePanel from '../components/SidePanel/SidePanel';
+import { Grid } from 'semantic-ui-react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      App
-    </div>
-  );
+    <Grid columns="equal" className="app" style={{ background: '#eee' }}>
+      <ColorPanel />
+      <SidePanel />
+
+      <Grid.Column style={{ marginLeft: 320 }}>
+        <Messages />
+      </Grid.Column>
+
+      <Grid.Column width={4}>
+        <MetaPanel />
+      </Grid.Column>
+    </Grid>
+  )
 }
 
 export default App;
