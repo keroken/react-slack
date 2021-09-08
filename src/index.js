e3d19d5ff4134e01d9e3791e60e137b35362c0a5
keroken <kenichi.shinagawa@gmail.com>
2021-09-08 23:41:49 +0900

    delete strict mode

--- src/index.js
+++ src/index.js
@@ -54,13 +54,11 @@ const RootWithAuth = withRouter(
 );
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <Router>
         <RootWithAuth />
       </Router>
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 
