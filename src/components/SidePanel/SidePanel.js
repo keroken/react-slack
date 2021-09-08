6ec07a32085bae2d8c68c951b31bb4a7578e0da5
keroken <kenichi.shinagawa@gmail.com>
2021-09-08 23:43:19 +0900

    pass user down to UserPanel

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -4,6 +4,8 @@ import UserPanel from './UserPanel.js';
 
 class SidePanel extends React.Component {
   render() {
+    const { currentUser } = this.props;
+
     return (
       <Menu
         size="large"
@@ -12,7 +14,7 @@ class SidePanel extends React.Component {
         vertical
         style={{ background: '#4c3c4c', fontSize: '1.2em' }}
       >
-        <UserPanel />
+        <UserPanel currentUser={currentUser} />
       </Menu>
     )
   }
