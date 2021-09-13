87ee49633fe0690a53487717467655c7139a9063
keroken <kenichi.shinagawa@gmail.com>
2021-09-13 23:55:54 +0900

    build Messages

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Segment, Comment } from 'semantic-ui-react';
+import MessagesHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
 
 class Messages extends React.Component {
   render() {
     return (
-      <div>Messages</div>
+      <>
+      <MessagesHeader />
+      <Segment>
+        <Comment.Group className="messages">
+          {/* Messages */}
+        </Comment.Group>
+      </Segment>
+      <MessageForm />
+      </>
     )
   }
 }
