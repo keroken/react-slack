2d07f0d5424c2ab7a879ad815f8b3cdb264ceae3
keroken <kenichi.shinagawa@gmail.com>
2021-09-14 23:40:22 +0900

    MessageForm - add functionality

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,8 +1,60 @@
 import React from 'react';
 import { Segment, Button, Input } from 'semantic-ui-react';
+import firebase from '../../firebase';
 
 
 class MessageForm extends React.Component {
+  state = {
+    message: '',
+    channel: this.props.currentChannel,
+    user: this.props.currentUser,
+    loading: false, 
+    errors: []
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  createMessage = () => {
+    const message = {
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      user: {
+        id: this.state.user.uid,
+        name: this.state.user.displayName,
+        avatar: this.state.user.photoURL
+      },
+      content: this.state.message
+    };
+    return message;
+  }
+
+  sendMessage = () => {
+    const { messageRef } = this.props;
+    const { message, channel } = this.state;
+    if (message) {
+      this.setState({ loading: true });
+      messageRef
+        .child(channel.id)
+        .push()
+        .set(this.createMessage())
+        .then(() => {
+          this.setState({ loading: false, message: '', errors: [] });
+        })
+        .catch(err => {
+          console.log(err);
+          this.setState({
+            loading: false,
+            errors: this.state.errors.concat(err)
+          });
+        })
+    } else {
+      this.setState({
+        errors: this.state.errors.concat({ message: 'Add a message' })
+      })
+    }
+  }
+
   render() {
     return (
       <Segment className="message__form">
