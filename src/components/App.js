1b47415501ac1611162e3b0f66aa789f14614e0b
keroken <kenichi.shinagawa@gmail.com>
2021-09-14 23:38:20 +0900

    App.js - pass down channel state to Messages

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,21 @@ import { Grid } from 'semantic-ui-react';
 import './App.css';
 import { connect } from 'react-redux';
 
-const App = ({ currentUser }) => {
+const App = ({ currentUser, currentChannel }) => {
   return (
     <Grid columns="equal" className="app" style={{ background: '#eee' }}>
       <ColorPanel />
-      <SidePanel currentUser={currentUser} />
+      <SidePanel
+        key={currentUser && currentUser.uid} // when passing multiple props you need unique key
+        currentUser={currentUser}
+      />
 
       <Grid.Column style={{ marginLeft: 320 }}>
-        <Messages />
+        <Messages
+          key={currentChannel && currentChannel.id} // when passing multiple props you need unique key
+          currentChannel={currentChannel}
+          currentUser={currentUser}
+        />
       </Grid.Column>
 
       <Grid.Column width={4}>
@@ -25,7 +32,8 @@ const App = ({ currentUser }) => {
 }
 
 const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+  currentUser: state.user.currentUser, 
+  currentChannel: state.channel.currentChannel
 });
 
 export default connect(mapStateToProps)(App);
