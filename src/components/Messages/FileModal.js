292bcde7c02b762e5f4b40d63abb3940943541cb
keroken <kenichi.shinagawa@gmail.com>
2021-11-27 01:05:44 +0900

    fix sendFile function

--- src/components/Messages/FileModal.js
+++ src/components/Messages/FileModal.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import mime from 'mime-types';
 import { Modal, Input, Button, Icon } from 'semantic-ui-react';
 
 class FileModal extends React.Component {
+  state = {
+    file: null,
+    authorized: ['image/jpeg', 'image/png']
+  }
+
+  addFile = event => {
+    const file = event.target.files[0];
+    if (file) {
+      this.setState({ file });
+    }
+  }
+
+  sendFile = () => {
+    const { file } = this.state;
+    const { uploadFile, closeModal } = this.props;
+
+    if (file !== null) {
+      console.log(mime);
+      if (this.isAuthorized(file.name)) {
+        const metadata = { contentType: mime.lookup(file.name) };
+        uploadFile(file, metadata);
+        closeModal();
+        this.clearFile();
+      }
+    }
+  }
+
+  isAuthorized = filename => this.state.authorized.includes(mime.lookup(filename));
+
+  clearFile = () => this.setState({ file: null });
+
   render() {
     const { modal, closeModal } = this.props;
 
@@ -11,15 +43,17 @@ class FileModal extends React.Component {
         <Modal.Content>
           <Input
             fluid
-            label="file"
+            label="File types: jpg, png"
             name="file"
             type="file"
+            onChange={this.addFile}
           />
         </Modal.Content>
         <Modal.Actions>
           <Button
             color="green"
             inverted
+            onClick={this.sendFile}
           >
             <Icon name="checkmark" /> Send
           </Button>
